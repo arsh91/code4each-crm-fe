ee33fef7d2b80823fb4210ec7d4c4cb9934466d1
simran-code4each <simranjit@code4each.com>
2024-02-28 12:24:54 +0530

    test changes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -12,6 +12,7 @@ import "@/assets/css/style.css";
 import "@/assets/js/cdn.jsdelivr.net_npm_bootstrap@5.0.2_dist_js_bootstrap.bundle.min.js";
 import "@/assets/js/code.jquery.com_jquery-3.6.4.min.js";
 </script>
+
 <template>
   <Dashboard />
 </template>
